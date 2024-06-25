42153ca4aed1f44c19220c150267f5154558fa09
S-reyapandey <shreyaamt02@gmail.com>
2024-06-25 20:47:36 +0530

    server setup

---
+++ blog-server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import 'dotenv/config';
+
+const app = express();
+
+const DB = process.env.DBURI.replace("<PASSWORD>", process.env.DBPASSWORD);
+
+mongoose
+  .connect(DB, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    // useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("DB connection is successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const port = process.env.PORT || 8000;
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
