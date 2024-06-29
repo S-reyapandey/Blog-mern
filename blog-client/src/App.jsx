52b61a4d856a39defb65a4ca2d6178a9ddae6bf4
S-reyapandey <shreyaamt02@gmail.com>
2024-06-29 17:42:26 +0530

    profile

--- blog-client/src/App.jsx
+++ blog-client/src/App.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from './pages/SignIn';
-import About from './pages/About';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Dashboard from './pages/Dashboard';
-import Header from './components/Header';
-import SignUp from './pages/SignUp';
-import FooterCom from './components/Footer';
-import Scrollbar from './components/Scrollbar';
-import ForgetP from './pages/ForgetP';
-import ResetP from './pages/ResetP';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
+import FooterCom from "./components/Footer";
+import Scrollbar from "./components/Scrollbar";
+import ForgetP from "./pages/ForgetP";
+import ResetP from "./pages/ResetP";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Scrollbar/>
-      <Header/>
+      <Scrollbar />
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/signin' element={<SignIn/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/project' element={<Projects/>}/>
-        <Route path='/forget' element={<ForgetP/>}/>
-        <Route path='/reset' element={<ResetP/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/home' element={<Home/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/project" element={<Projects />} />
+        <Route path="/forget" element={<ForgetP />} />
+        <Route path="/reset" element={<ResetP />} />
+        <Route path="/home" element={<Home />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
-       <FooterCom/>
+      <FooterCom />
     </BrowserRouter>
-      
-  )
+  );
 }
-
