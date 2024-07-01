49171b55f6f1bcdeab0359d48b6959ba4331d5d4
S-reyapandey <shreyaamt02@gmail.com>
2024-07-01 14:29:54 +0530

    commentsection

--- blog-server/routes/commentRoute.js
+++ blog-server/routes/commentRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, deleteComment, editComment, getpostcomments, likeComment } from '../controllers/commentController.js';
+import { createComment, deleteComment, editComment, getcomments, getpostcomments, likeComment } from '../controllers/commentController.js';
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.get('/getpostcomments/:postId', getpostcomments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
 router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments', verifyToken, getcomments);
 
 export default router;
